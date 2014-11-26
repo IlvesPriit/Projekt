70c713b44c8a0d50ba98e5636b63e56bf5c6a64f
OlenKarin <karin.kruup16@gmail.com>
2014-11-26 18:13:29 +0200

    muutsin seda kujunduse nuppu. Vaja ühte funktsooni nüüd kohendada, et see ka tööle hakkaks!

--- Projekt.py
+++ Projekt.py
@@ -46,7 +46,10 @@ def do_do(raam):
 raam = Tk()
 raam.title("To Do")
 colour = 'SkyBlue1'
-raam.configure(background=colour)
+
+def akna_värv(raam):
+    raam.configure(background=colour)
+akna_värv(raam)
 
 f = open("backg.gif")
 #background_image=ImageTK.Photoimage("backg.gif")
@@ -57,42 +60,19 @@ f = open("backg.gif")
 #img = raam.create_image(450, 80, image=bg)
 
 #rea pealkiri
+font = "Broadway"
 rea_pealkiri = Label(raam, text="Tegevus")
 rea_pealkiri.grid(column=0, row=0, padx=5, pady=5)
 rea_pealkiri.configure(background=colour)
-#sisestamise kastikesed
-tegevuse_sisestamine = Entry(raam, cursor="pencil")
-tegevuse_sisestamine.grid(column=2, row=0, padx=10, pady=10)
 
-aja_sisestamine = Entry(raam, cursor="pencil")
+#sisestamise kastikesed
+tegevuse_sisestamine = Entry(raam, cursor="pencil", font=font,justify=CENTER)
+tegevuse_sisestamine.grid(column=2, row=0, padx=10, pady=10)
+aja_sisestamine = Entry(raam, cursor="pencil",font=font, justify=CENTER)
 aja_sisestamine.grid(column=3, row=0, padx=10, pady=10)
 
-nupp = Button(raam, cursor="hand2",text=" Lisa ",bg="SkyBlue2", command=lambda: do_do(raam))
-#sulgudesse vaja ka command = alustab aja lugemist, mis on funktsioonis aeg
+nupp = Button(raam, cursor="hand2",text="  Lisa  ",bg="SkyBlue2", command=lambda: do_do(raam))
 nupp.grid(column=4, row= 0, padx=5, pady=6)
-
-#teen selle et värvi saaks valida
-#menüü buttoni moodi asi
-akna_colour = ttk.Menubutton(raam, cursor="hand2", text="Värv")
-akna_colour.grid(column=6, row= 0, padx=5, pady=5)
-akna_colour.menu= Menu(akna_colour, tearoff=0)
-akna_colour['menu'] = akna_colour.menu
-
-akna_colour.mayoVar  = IntVar()
-akna_colour.ketchVar = IntVar()
-akna_colour.menu.add_checkbutton(label='   ', background ="medium orchid", variable=akna_colour.mayoVar)
-akna_colour.menu.add_checkbutton(label='   ', background="blue",variable=akna_colour.ketchVar)
-akna_colour.menu.add_checkbutton(label='   ', background="pink",variable=akna_colour.ketchVar)
-akna_colour.menu.add_checkbutton(label='   ', background="white",variable=akna_colour.ketchVar)
-akna_colour.menu.add_checkbutton(label='   ', background="green yellow",variable=akna_colour.ketchVar)
-akna_colour.menu.add_checkbutton(label='   ', background="deep sky blue",variable=akna_colour.ketchVar)
-akna_colour.menu.add_checkbutton(label='   ', background="IndianRed1",variable=akna_colour.ketchVar)
-
-
-
-#akna_colour.add_command(label="Tulevikus", command=donothing)
-#akna_colour.add_command(label="Tulevikus", command=donothing)
-
 
 
 # soovime, et akna suuruse muutmisel muudetakse veeru 1 ja rea 1 mõõtmeid
@@ -111,11 +91,24 @@ raam.config(menu=menu)
 filemenu = Menu(menu)
 menu.add_cascade(label="Homme",menu=filemenu)
 #see lisab sinna menüü alla mingeid vidinaid
-#filemenu.add_command(label="Tulevikus", command=NewFile)
 menu.add_cascade(label="Tulevikus", menu=filemenu)
 menu.add_cascade(label="Arhiiv", menu=filemenu)
+#kujunduse kompott
+colors = Menu(menu)
+menu.add_cascade(label="Kujundus", menu=colors)
+colors.add_command(label='   ', background ="medium orchid", command=Uus_fail)
+colors.add_command(label='   ', background ="SkyBlue1", command=Uus_fail)
+colors.add_command(label='   ', background="blue",command=Uus_fail)
+colors.add_command(label='   ', background="pink",command=Uus_fail)
+colors.add_command(label='   ', background="white",command=Uus_fail)
+colors.add_command(label='   ', background="green yellow",command=Uus_fail)
+colors.add_command(label='   ', background="deep sky blue",command=Uus_fail)
+colors.add_command(label='   ', background="IndianRed1",command=Uus_fail)
 
-filemenu.add_separator()
+
+
+
+#filemenu.add_separator()
 filemenu.add_command(label="Exit", command=raam.quit)
 # kuvame akna ekraanile
 raam.mainloop()
--- lisa_to_do.py
+++ lisa_to_do.py
@@ -6,15 +6,15 @@ from stopp_nupp import stopp
 
 #funktsioon üheks linnukesega tegevuse reaks
 #see värk peaks käivituma iga kord kui vajutada nuppu 'lisa'
-def do_do(raam, colour):
+def do_do(raam, colour, font):
     var = IntVar()
     c = Checkbutton(raam, variable=var)
     c.grid(column=0, padx=5, pady=5)
     rida= c.grid_info()["row"]
-    tegevus = ttk.Label(raam, text="tegevus")
+    tegevus = ttk.Label(raam, text="tegevus", font=font)
     tegevus.grid(column=2, row = rida, padx=5, pady=5)
     tegevus.configure(background=colour)
-    aeg = ttk.Label(raam, text="aeg")
+    aeg = ttk.Label(raam, text="aeg", font=font)
     aeg.grid(column=3, row = rida,  padx=5, pady=5)
     aeg.configure(background=colour)
     # soovime, et nupp veniks nii laiuses kui ka kõrguses
